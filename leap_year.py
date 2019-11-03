278076a22bc1bfc9f9b381a6a7ae970a5993f487
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 13:43:01 -0700

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,10 +4,14 @@
 #Unit 8 Labs
 #Leap Year
 
+#Get user input for year
 year = int(input("Enter a year: "))
 
+#If year is divisible by 4
 if year % 4 == 0:
+    #If year is divisible by 100
     if year % 100 == 0:
+        #If year is divisible by 400
         if year % 400 == 0:
             print("True")
         else:
